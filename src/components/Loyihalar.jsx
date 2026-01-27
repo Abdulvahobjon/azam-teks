89aa88f73ee87f849059af8458d27f40e0ede7be
“Oybek” <sobirv710@gmail.com>
2026-01-27 09:45:31 +0500

    test2

--- src/components/Loyihalar.jsx
+++ src/components/Loyihalar.jsx
@@ -7,7 +7,6 @@ export default function Loyihalar(){
 	return(
 		<>
 			<Section5/>
-			<Footer/>
 		</>
 	)
 }
--- src/components/Xizmatlar.jsx
+++ src/components/Xizmatlar.jsx
@@ -7,7 +7,6 @@ export default function Xizmatlar(){
 	return(
 		<>
 			<Section4/>
-			<Footer/>
 		</>
 	)
 }
