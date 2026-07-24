f00434ecf3d4e1ce03c3f4f1664039668fef004a
“Oybek” <sobirv710@gmail.com>
2026-07-24 16:05:58 +0500

    Phone numbers fixed commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,7 +35,8 @@ export default function Footer() {
         <div className="footer-section contact">
           <h4>{t("Manzil")}</h4>
           <p>160105, Namangan, Balandariq 6A berk koʻchasi, 42</p><br />
-          <span><FaPhone/>(90) 222-78-77</span>
+          <span><FaPhone/>90 222-78-77 | Bosh menedjer</span>
+          <span><FaPhone/>90 218-38-78 | Direktor</span>
           <Link to={"https://e.mail.ru/compose/?to=azam-teks-mchj%40mail.ru"}><MdEmail/>azam-teks-mchj@mail.ru</Link>
           <span></span>
           <Link to={"https://www.google.com/maps/place/AZAM+TEKS,+160105,+Namangan,+Namangan+viloyati,+%D0%A3%D0%B7%D0%B1%D0%B5%D0%BA%D0%B8%D1%81%D1%82%D0%B0%D0%BD/@41.024276,71.65132,15z/data=!4m6!3m5!1s0x38bb4bbf96db9a55:0x114b1555b7e0a372!8m2!3d41.0241572!4d71.6506983!16s%2Fg%2F11v5dkm92h?hl=ru&gl=US&g_ep=Eg1tbF8yMDI2MDExNF8wIOC7DCoASAJQAg%3D%3D"}><IoLocationSharp/>{t("Joylashuv")}</Link>
